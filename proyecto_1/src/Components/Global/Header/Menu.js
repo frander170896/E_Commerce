06dd5e49b8adcd12cb1079a9898b231d8272bcd7
frander170896 <franderramirez@yahoo.es>
2018-09-01 20:29:03 -0600

    cambios

--- proyecto_1/src/Components/Global/Header/Menu.js
+++ proyecto_1/src/Components/Global/Header/Menu.js
@@ -12,13 +12,16 @@ class Menu extends Component {
                     <a className="nav-link" href="category.html">Categories</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="Products">Products</a>
+                    <a className="nav-link" href="/Products">Products</a>
                 </li>
                 <li className="nav-item active">
-                    <a className="nav-link" href="contact.html">Cart <span className="sr-only">(current)</span></a>
+                    <a className="nav-link" href="/cart">Cart <span className="sr-only">(current)</span></a>
                 </li>
                 <li className="nav-item">
                     <a className="nav-link" href="contact.html">Contact</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/about_us">About us</a>
                 </li>
             </ul>
         </div>
--- proyecto_1/src/routes.js
+++ proyecto_1/src/routes.js
@@ -8,14 +8,15 @@ import ProductList from './Components/Products/ProductList';
 import Header from './Components/Global/Header/Header'
 import Cart from './Components/Cart/Cart.js'
 import Footer from './Components/Global/Footer/Footer'
-
+import About from './Components/About_us/About_us'
 
 const AppRoutes = () =>
   <App>
     <Switch>
-      <Route exact path="/" component={Cart} />
+      <Route exact path="/cart" component={Cart} />
       <Route exact path="/about" component={Header} />
       <Route exact path="/contact" component={Footer} />
+      <Route exact path="/about_us" component={About} />
       <Route exact path="/Products" component={ProductList} />
     </Switch>
   </App>;
