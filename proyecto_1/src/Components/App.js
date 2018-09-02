adbb716d607753b72f4250a2baa1829e511c022e
Gerson Vargas <gersonvargasgalvez@gmail.com>
2018-09-01 18:19:54 -0600

    routes

--- proyecto_1/src/Components/App.js
+++ proyecto_1/src/Components/App.js
@@ -1,22 +1,27 @@
+// Dependencies
 import React, { Component } from 'react';
-import './Global/css/App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-//Components
-import Header from './Global/Header/Header'
-import Cart from './Cart/Cart.js'
-import Footer from './Global/Footer/Footer'
+// Components
+import Header from './Global/Header/Header';
+import Content from './Global/Content';
+import Footer from './Global/Footer/Footer';
+
 
 class App extends Component {
+  static propTypes = {
+    children: PropTypes.object.isRequired
+  };
+
   render() {
+    const { children } = this.props;
+
     return (
-      <Router>
-    <div>
-        <Header></Header>
-        <Cart></Cart>
-        <Footer></Footer>
-    </div>
-    </Router>
+      <div className="container">
+        <Header/>
+        <Content body={children} />
+        <Footer/>
+      </div>
     );
   }
 }
---
+++ proyecto_1/src/Components/Global/Content.js
@@ -0,0 +1,28 @@
+// Dependencies
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+//import Cart from '../Cart/Cart';
+
+// Assets
+
+
+class Content extends Component {
+  static propTypes = {
+    body: PropTypes.object.isRequired
+  };
+
+  render() {
+    const { body } = this.props;
+
+    return (
+      <div className = "row" >
+        <div className="Content margenes-contenido">
+        {body}
+      </div>
+      </div>
+    );
+  }
+}
+
+export default Content;
