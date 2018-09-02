aa348f28cd60fe28d6f6c675adb4193c9d8b782f
frander170896 <franderramirez@yahoo.es>
2018-09-02 13:42:19 -0600

    About_us

--- proyecto_1/src/Components/About_us/About_us.js
+++ proyecto_1/src/Components/About_us/About_us.js
@@ -3,11 +3,14 @@ import React,{Component} from 'react';
 
 //Componets
 import Info from './Info.js'
+import CardGroup from './CardGroup.js'
 class About extends Component{
+
     render (){
         return (
             <div>
                 <Info></Info>
+                <CardGroup></CardGroup>
             </div>
         );
     }
