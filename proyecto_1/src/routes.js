cf51b2658251b776fc523459750c5c45d556a991
Gerson Vargas <gersonvargasgalvez@gmail.com>
2018-09-01 20:40:24 -0600

    routes

--- proyecto_1/src/routes.js
+++ proyecto_1/src/routes.js
@@ -5,6 +5,7 @@ import { Route, Switch } from 'react-router-dom';
 //Components
 import App from './Components/App';
 import ProductList from './Components/Products/ProductList';
+import Home from './Components/Home/Home';
 import Header from './Components/Global/Header/Header'
 import Cart from './Components/Cart/Cart.js'
 import Footer from './Components/Global/Footer/Footer'
@@ -17,6 +18,7 @@ const AppRoutes = () =>
       <Route exact path="/about" component={Header} />
       <Route exact path="/contact" component={Footer} />
       <Route exact path="/Products" component={ProductList} />
+      <Route exact path="/Home" component={Home} />
     </Switch>
   </App>;
 
