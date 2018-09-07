9095d62fe63079c19147be887967bf5433b32ff4
David Padilla <depm100792@gmail.com>
2018-09-07 11:40:58 -0600

    DB sql

---
+++ DB.sql
@@ -0,0 +1,36 @@
+CREATE SCHEMA `implementosdeportivos` ;
+
+use implementosdeportivos;
+
+CREATE TABLE `implementosdeportivos`.`articulos` (
+id bigint(20) NOT NULL AUTO_INCREMENT,
+precio int(11) DEFAULT NULL,
+descripcion varchar(255) DEFAULT NULL,
+nombre varchar(255) DEFAULT NULL,
+estado int(11) NOT NULL,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE `implementosdeportivos`.`compras` (
+id bigint(20) NOT NULL AUTO_INCREMENT,
+total int(11) DEFAULT NULL,
+subtotal int(11) DEFAULT NULL,
+estado int(11) NOT NULL,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE `implementosdeportivos`.`comprasArticulos` (
+id bigint(20) NOT NULL AUTO_INCREMENT,
+compra bigint(20) DEFAULT NULL,
+articulo bigint(20) DEFAULT NULL,
+PRIMARY KEY (id),
+CONSTRAINT FKarticulo FOREIGN KEY (articulo) REFERENCES articulos (id),
+CONSTRAINT FKcompra FOREIGN KEY (compra) REFERENCES compras (id)
+);
+
+CREATE TABLE `implementosdeportivos`.`usuarios` (
+id bigint(20) NOT NULL AUTO_INCREMENT,
+usuario varchar(255) DEFAULT NULL,
+contraseña varchar(255) DEFAULT NULL,
+PRIMARY KEY (id)
+);
