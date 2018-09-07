07b7016e6cd95e70953994b813329c715a8e2732
Gerson Vargas <gersonvargasgalvez@gmail.com>
2018-09-06 20:55:01 -0600

    contact textarea

--- proyecto_1/src/Components/Contact/Contact.js
+++ proyecto_1/src/Components/Contact/Contact.js
@@ -31,7 +31,6 @@ Los usuarios tendrán múltiple opciones de filtrado según sus necesidades o su
                             <legend className="scheduler-border">Register</legend>
 
                                 <div className="row">
-
                                     <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                                         <input className="form-control" type="text" placeholder="Full Name" required/>
                                     </div>
@@ -56,49 +55,13 @@ Los usuarios tendrán múltiple opciones de filtrado según sus necesidades o su
 
                                 <div className="row">
                                     <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                                        <input className="form-control" type="password" placeholder="Password" required/>
+                                    <textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
                                     </div>
                                 </div>
 
                                 <div className="row">
                                     <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                                        <input className="form-control" type="password" placeholder="Confirm Password" required/>
-                                    </div>
-                                </div>
-
-                                <div className="row">
-                                    <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                                        <input className="form-control" type="text" placeholder="Job" required/>
-                                    </div>
-                                </div>
-
-                                <div className="row">
-                                    <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                                        <select className="form-control" id="province" name="province" required>
-                                            <option value="">Select a province</option>
-                                            <option value="1">San José</option>
-                                            <option value="2">Alajuela</option>
-                                            <option value="3">Heredia</option>
-                                            <option value="4">Cartago</option>
-                                            <option value="5">Guanacaste</option>
-                                            <option value="6">Puntarenas</option>
-                                            <option value="7">Limón</option>
-                                        </select>
-                                    </div>
-                                </div>
-
-                                <div className="row">
-                                    <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                                        <div className="custom-file">
-                                            <input type="file" className="custom-file-input" name="file" required/>
-                                            <label className="custom-file-label" htmlFor="customFile">Attach CV</label>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div className="row">
-                                    <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                                        <button className="btn btn-primary" type="button" name="finalize">Finalize</button>
+                                        <button className="btn btn-primary" type="button" name="finalize">Send</button>
                                     </div>
                                 </div>
                             </fieldset>
