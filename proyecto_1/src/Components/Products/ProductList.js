63a3f80d2a8c75b340a290e67b866cf91672f9bd
Gerson Vargas <gersonvargasgalvez@gmail.com>
2018-09-07 20:07:22 -0600

    filtrado

--- proyecto_1/src/Components/Products/ProductList.js
+++ proyecto_1/src/Components/Products/ProductList.js
@@ -6,16 +6,18 @@ class ProductList extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      server_url:'http://localhost/Proyectos/E_Commerce/',
+      server_url: 'http://localhost/Proyectos/E_Commerce/',
       api: 'http://localhost/Proyectos/E_Commerce/server/index.php',
-      Cart: []
+      Cart: [],
+      datosFiltrados: null,
+      isfiltrado: false
     // jobs: this.props.job_list
     }
 
     this.componentDidMount = this.componentDidMount.bind(this)
     this.handleClickAddCart = this.handleClickAddCart.bind(this)
     this.componentWillMount = this.componentWillMount.bind(this)
-
+    this.filterList = this.filterList.bind(this)
   // console.log(this.state.jobs[0])
   }
   componentDidMount () {
@@ -27,9 +29,9 @@ class ProductList extends Component {
   // aqui se debe de llamar el metodo que consume el web service con el fin de que envie los datos del articulo seleccionado y lo agregue a la variable de session.
   }
   filterList (event) {
-    var updatedList = this.state.jobs
+    var updatedList = this.state.Cart
     updatedList = updatedList.filter(function (item) {
-      return item.title.toLowerCase().search(
+      return item.nombre.toLowerCase().search(
           event.target.value.toLowerCase()) !== -1
     })
     this.setState({
@@ -69,19 +71,40 @@ class ProductList extends Component {
             placeholder='Search'
             onChange={this.filterList} />
         </div>
-        {this.state.Cart.map(elemento => <div key={elemento.id}>
-                             <ProductItem
-                               jobtitle={elemento.nombre}
-                               //id: "1", precio: "1000", descripcion: "Art1 desc", nombre: "Art1", estado: "1"
-                               nombre={elemento.nombre}
-                               precio={"₡"+elemento.precio}
-                               descripcion={elemento.descripcion}
-                               server_url={this.state.server_url}                               
-                               imagen={elemento.imagen}
-                               creado={elemento.fecha_registro}
-                               evento={this.handleClickAddCart} />
-                           </div>
-                            )}
+        {!this.state.Cart && !this.state.isfiltrado ?
+                            <div className="alert alert-primary" role="alert">
+                                <i><strong>Loading...</strong></i>
+                            </div>
+                            :
+                            !this.state.isfiltrado && !this.state.datosFiltrados ?
+                            this.state.Cart.map(elemento => <div key={elemento.id}>
+                                                      <ProductItem
+                                                        jobtitle={elemento.nombre}
+                                                        //id: "1", precio: "1000", descripcion: "Art1 desc", nombre: "Art1", estado: "1"
+                                                        nombre={elemento.nombre}
+                                                        precio={"₡"+elemento.precio}
+                                                        descripcion={elemento.descripcion}
+                                                        server_url={this.state.server_url}                               
+                                                        imagen={elemento.imagen}
+                                                        creado={elemento.fecha_registro}
+                                                        evento={this.handleClickAddCart} />
+                                                    </div>
+                                                     )
+                                                    :
+                                                    this.state.datosFiltrados.map(elemento => <div key={elemento.id}>
+                                                      <ProductItem
+                                                        jobtitle={elemento.nombre}
+                                                        //id: "1", precio: "1000", descripcion: "Art1 desc", nombre: "Art1", estado: "1"
+                                                        nombre={elemento.nombre}
+                                                        precio={"₡"+elemento.precio}
+                                                        descripcion={elemento.descripcion}
+                                                        server_url={this.state.server_url}                               
+                                                        imagen={elemento.imagen}
+                                                        creado={elemento.fecha_registro}
+                                                        evento={this.handleClickAddCart} />
+                                                    </div>
+                                                     )
+                                                    }
       </div>
 
     )
