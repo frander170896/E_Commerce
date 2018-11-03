6ff205f18aae0d225d68391c347ac46245522c27
Gerson Vargas <gersonvargasgalvez@gmail.com>
2018-11-03 09:11:18 -0600

    noticias

---
+++ proyecto_1/src/Components/Noticias/NoticiaItem.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react';
+
+import Company from "../Global/images/logo.svg";
+
+class ProductDetail extends Component {
+    render() {
+        return (
+            <img src={this.props.company_logo ?
+                this.props.server_url+this.props.company_logo : Company} 
+                 className="img-thumbnail img-company" alt="company" />
+        );
+    }
+}
+
+class ApplyLink extends Component {
+    render() {
+        return (
+            <a className='active btn btn-link text-white' target="_blank" href={this.props.company_url}>
+                {this.props.action_name}
+            </a>
+        );
+    }
+}
+
+class NoticiaItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+            show: false
+        };
+        this.handleShow = this.handleShow.bind(this);
+    }
+    handleShow() {
+        this.setState({ show: true });
+    }
+    generarArray(id,nombre,descripcion,precio,creado){
+        const array = {id,nombre,descripcion,precio,creado}
+        return JSON.stringify(array);
+    }
+    handleDetails(id,nombre,descripcion,precio,creado) { // se reciben los datos del articulo para enviarselos al padre
+        const array = this.generarArray(id,nombre,descripcion,precio,creado);
+        this.props.evento(array);
+    }
+    render() {
+        return (
+            <div>
+              
+
+                <div className="row p-2">
+                    <div className="col-sm-3">
+                        <div className="card m-0  card-job">
+                            <div className="card-body">
+                                <p>
+                                    <b>{this.props.nombre}</b>
+                                </p>
+                                <ProductDetail show={this.state.show}
+                                visible={this.state.show}
+                                server_url={this.props.server_url}
+                                company_logo={this.props.imagen} /> 
+                            </div>
+                            <p>
+                                <i>{this.props.company_description}
+                                    <ApplyLink company_url={this.props.imagen} action_name={'Visit Us'} />
+                                </i>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="col-sm-9 ">
+                        <div className="card  card-job">
+                            <div className="card-body">
+                                <h4 className="card-title">Product</h4>
+                                <h5 className="card-subtitle mb-2">{this.props.nombre}</h5>
+                                <p> <i>
+                                    <strong>Description:  </strong>
+                                     {this.props.descripcion ? this.props.descripcion : 'Not especified.'}
+                                </i>
+                                </p>
+                                <p>
+                                    <i><strong>Job Type:  </strong> {"₡"+this.props.precio}</i>
+                                </p>
+                                <p>
+                                    <i><strong>Items:  </strong> {this.props.cantidad}</i>
+                                </p>
+                                <p className="card-text">
+                                <i><strong>Last Modified:  </strong> {this.props.creado}</i>
+                                
+                                </p>
+                                <button className="btn btn-danger"
+                                 onClick={()=>{this.handleDetails(this.props.id,this.props.nombre,
+                                 this.props.descripcion,
+                                 this.props.precio,this.props.creado) 
+                                 }} >Add to cart</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default NoticiaItem;
---
+++ proyecto_1/src/Components/Noticias/Noticias.js
@@ -0,0 +1,135 @@
+import React, { Component } from 'react'
+import NoticiaItem from './NoticiaItem'
+import Lupa from '../Global/images/lupa.png'
+
+class Noticias extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            server_url: 'http://localhost:8098/Proyectos/E_Commerce/',
+            api: 'http://localhost:8098/Proyectos/E_Commerce/server/Controlador/index.php',
+            Cart: [],
+            productos: [],
+            datosFiltrados: null,
+            isfiltrado: false
+            // jobs: this.props.job_list
+        }
+
+        this.handleClickAddCart = this.handleClickAddCart.bind(this)
+        this.componentWillMount = this.componentWillMount.bind(this)
+        this.filterList = this.filterList.bind(this)
+        // console.log(this.state.jobs[0])
+    }
+    existInCart(cart, articulo) {
+        let existe = false
+        for (var i = 0; i < cart.length; i++) {
+            if (articulo.id == cart[i].id) {
+                return true
+            }
+        }
+        return false
+    }
+    saveCartLocalStorage(Aritculo) {
+        let articulo = JSON.parse(Aritculo)
+        let cart = localStorage.Cart ? JSON.parse(localStorage.Cart) : []
+        console.log(cart)
+        if (!this.existInCart(cart, articulo)) {
+            cart.push({ id: articulo.id, nombre: articulo.nombre, descripcion: articulo.descripcion, precio: articulo.precio, creado: articulo.creado });
+            localStorage.setItem('Cart', JSON.stringify(cart))
+        }
+        console.log(cart)
+    }
+    handleClickAddCart(data) {
+        this.saveCartLocalStorage(data);
+        window.location.reload()
+        // aqui se debe de llamar el metodo que consume el web service con el fin de que envie los datos del articulo seleccionado y lo agregue a la variable de session.
+    }
+    filterList(event) {
+        var updatedList = this.state.productos
+        updatedList = updatedList.filter(function (item) {
+            return item.nombre.toLowerCase().search(
+                event.target.value.toLowerCase()) !== -1
+        })
+        this.setState({
+            datosFiltrados: updatedList,
+            isfiltrado: true
+        })
+    }
+    componentWillMount() {
+        var url = this.state.api +
+            '/implDeport/'
+        try {
+            fetch(url)
+                .then((response) => {
+                    return response.json()
+                })
+                .then((data) => {
+                    if (data) {
+                        //console.log(data)
+                        this.setState({ productos: data })
+                    } else {
+                    }
+                })
+        } catch (err) { }
+    }
+
+    render() {
+        return (
+            <div>
+                <h2 className='ml-3 mt-3'>Available products</h2>
+                <div className='input-group-prepend m-3'>
+                    <span className='input-group-text' id='inputGroup-sizing-default'><img src={Lupa} /></span>
+                    <input
+                        className='form-control'
+                        aria-label='Default'
+                        aria-describedby='inputGroup-sizing-default'
+                        type='text'
+                        placeholder='Search'
+                        onChange={this.filterList} />
+                </div>
+                {!this.state.productos && !this.state.isfiltrado ?
+                    <div className="alert alert-primary" role="alert">
+                        <i><strong>Loading...</strong></i>
+                    </div>
+                    :
+                    !this.state.isfiltrado && !this.state.datosFiltrados ?
+                        this.state.productos.map(elemento => <div key={elemento.id}>
+                            <NoticiaItem
+                                jobtitle={elemento.nombre}
+                                //id: "1", precio: "1000", descripcion: "Art1 desc", nombre: "Art1", estado: "1"
+                                nombre={elemento.nombre}
+                                precio={elemento.precio}
+                                cantidad={elemento.cantidad}
+                                descripcion={elemento.descripcion}
+                                server_url={this.state.server_url}
+                                imagen={elemento.imagen}
+                                creado={elemento.fecha_registro}
+                                id={elemento.id}
+                                evento={this.handleClickAddCart} />
+                        </div>
+                        )
+                        :
+                        this.state.datosFiltrados.map(elemento => <div key={elemento.id}>
+                            <NoticiaItem
+                                jobtitle={elemento.nombre}
+                                //id: "1", precio: "1000", descripcion: "Art1 desc", nombre: "Art1", estado: "1"
+                                nombre={elemento.nombre}
+                                precio={elemento.precio}
+                                cantidad={elemento.cantidad}
+                                descripcion={elemento.descripcion}
+                                server_url={this.state.server_url}
+                                imagen={elemento.imagen}
+                                creado={elemento.fecha_registro}
+                                id={elemento.id}
+                                evento={this.handleClickAddCart} />
+                        </div>
+                        )
+                }
+            </div>
+
+        )
+    }
+
+}
+
+export default Noticias
